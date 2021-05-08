60dde6ff8951414223490d81666ead9d3d375e76
Brian Foutty <8526059+Swift-Teacher@users.noreply.github.com>
2021-05-08 14:53:32 -0400

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Brian Foutty on 5/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
