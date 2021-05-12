3e56fcd36c6f9838085eab81f66a9b912c5e39da
Swift-Teacher <brian@swiftteacher.org>
2021-05-12 09:06:04 -0400

    Added call button code.

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -8,16 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let phoneNumber = "3308470503"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func callBrian(_ sender: Any) {
+        // this enables a physical device to call our phone number
+        if let url = NSURL(string: "tel://\(phoneNumber)") {
+            UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+            
+            // since the Simulator cannot make phone call. This line will print a string to the console to confirm the code executed.
+            print("The phone is calling my number. Voicemail would be triggered automatically if this were a physical device.")
+        }
+    }
+    
+    /*
     @IBAction func unwindToHome(unwindSegue: UIStoryboardSegue) {
         
-    }
-
+    }*/
 
 }
 
