4c9b03e9f0071f74670f46fa47251c0070bd4a1f
Swift-Teacher <brian@swiftteacher.org>
2021-05-13 08:53:26 -0400

    Added showWebPage function that creates a SafariViewController within the app.

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -4,10 +4,10 @@
 //
 //  Created by Brian Foutty on 5/8/21.
 //
-
+import SafariServices
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
     
     let phoneNumber = "3308470503"
 
@@ -23,6 +23,25 @@ class ViewController: UIViewController {
             
             // since the Simulator cannot make phone call. This line will print a string to the console to confirm the code executed.
             print("The phone is calling my number. Voicemail would be triggered automatically if this were a physical device.")
+        }
+    }
+    
+    @IBAction func skillsButtonTapped(_ sender: Any) {
+        showWebPage()
+    }
+    
+    
+    
+    func showWebPage() {
+        let urlString = "https://www.swiftteacher.org"
+        
+        if let url = URL(string: urlString) {
+            let config = SFSafariViewController.Configuration()
+            config.entersReaderIfAvailable = true
+            
+            let view = SFSafariViewController(url: url, configuration: config)
+            view.delegate = self
+            present(view, animated: true)
         }
     }
     
